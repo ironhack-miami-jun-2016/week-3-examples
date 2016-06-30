c017def7751e1f72a2758feec12d99ff25672f8b
Nizar Khalife Iglesias <khalifenizar@gmail.com>
2016-06-30 13:44:53 -0400

    Callbacks: Add reading file example

--- 4-callbacks/app.js
+++ 4-callbacks/app.js
@@ -1,7 +1,24 @@
+var fs = require("fs");
+
+
+fs.readFile("movies.txt", "utf8", function (theError, fileContents) {
+  console.log("\nFile has been read! Printing it's contents:");
+  // We have access to the contents of the file in here
+  console.log(fileContents);
+});
+
+
+
+
+
+
+// -----------------------------------------------------------------
+// ---------Sleep examples using setTimeout under the hood---------
+// -----------------------------------------------------------------
 var sleep = require("./lib/sleep.js");
 
 sleep(5, function () {
-  console.log("5 seconds have ellapsed");
+  console.log("\n5 seconds have ellapsed");
 });
 
 
@@ -11,6 +28,8 @@ var colorsArray = [ "chartreuse", "indigo", "orange", "black" ];
 sleep(1, function () {
 
   // Anonymous function style
+  console.log("\n1 second has ellapsed! Printing the array");
+
   colorsArray.forEach(function (theColor) {
     console.log(theColor.toUpperCase());
   });
@@ -18,4 +37,4 @@ sleep(1, function () {
 });
 
 
-console.log("END OF PROGRAM");
+console.log("\nEND OF PROGRAM\n");
