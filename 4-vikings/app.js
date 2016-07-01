6a43b47456f265280840cc48acbddf792b7c651d
Nizar Khalife Iglesias <khalifenizar@gmail.com>
2016-07-01 11:17:56 -0400

    Vikings: Add PitFight class

--- 4-vikings/app.js
+++ 4-vikings/app.js
@@ -1,8 +1,10 @@
 "use strict";
 
 var Viking = require("./lib/viking.js");
+var PitFight = require("./lib/pitfight.js");
 
 var viking1 = new Viking("Nizar", 200, 30);
 var viking2 = new Viking("Josh", 150, 20);
 
-viking1.attack(viking2);
+var thisPitFight = new PitFight(viking1, viking2, 10);
+thisPitFight.initiate();
