990eb65501ace1f9626cc42ba429128d9d21ef5f
Nizar Khalife Iglesias <khalifenizar@gmail.com>
2016-07-04 09:40:18 -0400

    Quiz: Add read recursive loop

--- 5-quiz-js/app.js
+++ 5-quiz-js/app.js
@@ -1,24 +1,38 @@
 "use strict";
 
+var read = require("read");
+
 var Question = require("./lib/question.js");
 var Quiz = require("./lib/quiz.js");
 
 var question1 = new Question(1, "What is the primary programming language on browsers?", "JavaScript");
 var question2 = new Question(2, "What allows you to run JavaScript on the backend?", "Node.js");
+var question3 = new Question(3, "What's the capital of Burkina Faso?", "Ouagadougou");
+var question4 = new Question(4, "How many stars are in the Milky Way Galaxy?", "100 billion");
+var question5 = new Question(5, "How many feet are in a mile?", "5280");
 
 var theQuiz = new Quiz();
 
 theQuiz.addQuestion( question1 );
 theQuiz.addQuestion( question2 );
+theQuiz.addQuestion( question3 );
+theQuiz.addQuestion( question4 );
+theQuiz.addQuestion( question5 );
 
-console.log( theQuiz.getCurrentQuestion() ===  "What is the primary programming language on browsers?");
-console.log( theQuiz.answer("JavaScript") === true );
-console.log( theQuiz.isGameOver() === false );
 
-console.log( theQuiz.getCurrentQuestion() === "What allows you to run JavaScript on the backend?" );
-console.log( theQuiz.answer("blah") === false );
-console.log( theQuiz.isGameOver() === false );
+function handleInput (theError, input) {
+  theQuiz.answer(input);
 
-console.log( theQuiz.getCurrentQuestion() === "What allows you to run JavaScript on the backend?" );
-console.log( theQuiz.answer("Node.js") === true );
-console.log( theQuiz.isGameOver() === true );
+  if (theQuiz.isGameOver()) {
+    console.log("GAME OVER");
+  }
+  else {
+    options.prompt = theQuiz.getCurrentQuestion();
+    read(options, handleInput);
+  }
+}
+
+
+var options = { prompt: theQuiz.getCurrentQuestion() };
+
+read(options, handleInput);
